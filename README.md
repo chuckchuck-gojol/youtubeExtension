cac8754deea706365e505914bbdc42ed822371af
mjung1798 <mor2222@naver.com>
2020-04-17 00:50:11 +0900

    Feat: youtube comment crawling : with python

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # youtubeExtension
+
+### 1. Chrome driver를 설치한다.
+
+<https://sites.google.com/a/chromium.org/chromedriver/downloads>
+
+chromedriver를 /usr/local/bind path에 있다고 설정해둔 상태이다.
+
+
+
+### 2. pip install을 이용해 모듈을 설치한다.
+
+pip install selenium
+
+pip install beautifulsoup
+
+> 빠른시일내에 requirement.txt 를 설치하도록 하겠다.
+
+
+
+### 3. 크롤링과정
+
+selenium + chrome driver 을 이용해 프로그램을 이용해 chrome 창이 열리며, 페이지를 끝까지 로딩한다 이후 beautifulsoup을 이용하여, 댓글 사용자명 : 댓글 내용을 print 한다.
+
---
+++ crawling.py
@@ -0,0 +1,52 @@
+from selenium import webdriver as wd
+from bs4 import BeautifulSoup
+import time
+import argparse
+
+parser = argparse.ArgumentParser(description='유튜브 댓글을 크롤링합니다.')
+parser.add_argument('--url', required=True, help='유튜브 주소를 입력해주세요')
+
+args = parser.parse_args()
+
+driver = wd.Chrome(executable_path="/usr/local/bin/chromedriver")
+url = args.url
+driver.get(url)
+
+last_page_height = driver.execute_script("return document.documentElement.scrollHeight")
+
+while True:
+    driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+    time.sleep(3.0)
+    new_page_height = driver.execute_script("return document.documentElement.scrollHeight")
+
+    if new_page_height == last_page_height:
+        break
+    last_page_height = new_page_height
+
+html_source = driver.page_source
+
+driver.close()
+
+soup = BeautifulSoup(html_source, 'lxml')
+
+youtube_user_IDs = soup.select('div#header-author > a > span')
+youtube_comments = soup.select('yt-formatted-string#content-text')
+
+str_youtube_userIDs = []
+str_youtube_comments = []
+
+for i in range(len(youtube_user_IDs)):
+    str_tmp = str(youtube_user_IDs[i].text)
+    # print(str_tmp)
+    str_tmp = str_tmp.replace('\n', '')
+    str_tmp = str_tmp.replace('\t', '')
+    str_tmp = str_tmp.replace(' ','')
+    str_youtube_userIDs.append(str_tmp)
+    str_tmp = str(youtube_comments[i].text)
+    str_tmp = str_tmp.replace('\n', '')
+    str_tmp = str_tmp.replace('\t', '')
+    str_tmp = str_tmp.replace(' ', '')
+    str_youtube_comments.append(str_tmp)
+
+for i in range(len(str_youtube_userIDs)):
+    print(str_youtube_userIDs[i], str_youtube_comments[i])
